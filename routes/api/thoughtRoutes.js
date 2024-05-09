7c9cd7bb29200abd6c2fee22c3f627fe8bbbc918
EmilioColds <emiliofriastapia2705@outlook.com>
2024-05-09 11:51:17 -0600

    UPDATE and DELETE thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -24,3 +24,52 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+//POST thought
+router.post('/', async (req, res) => {
+    try {
+        const newThought = await Thought.create(req.body);
+        await User.findByIdAndUpdate(
+            req.body.userId,
+            { $push: { thoughts: newThought._id } },
+            { new: true });
+        res.json(newThought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//UPDATE (PUT) a thought
+router.put('/:id', async (req, res) => {
+    try {
+        const updateThought = await Thought.findByIdAndUpdate(
+            req.params.id, 
+            req.body,
+            { new: true });
+        res.json(updateThought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//DELETE thought
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteThought = await Thought.findByIdAndDelete(req.params.id);
+        if (!deleteThought) {
+            res.status(404).json({ message: "There's no thought with that ID!" });
+            return;
+        }
+        await User.findByIdAndUpdate(thought.userId, 
+            { $pull: { thoughts: req.params.id } },
+            { new: true });
+        res.json({ message: 'Thought deleted successfully!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//POST reaction
+
+
+module.exports = router;
